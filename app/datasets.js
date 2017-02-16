817c37a058f562b7c431d299c67bf5bc82dd49b4
Michal Niedziela <MichalNiedziela1601@labnoratory.com>
2017-02-16 22:59:49 +0100

    test task2

--- app/datasets.js
+++ app/datasets.js
@@ -1,32 +1,56 @@
 'use strict';
+var _ = require('lodash');
 
 module.exports = {
     after: function (callback)
     {
-        return [];
-    },
-    bind: function (callback)
+        return [2, callback];
+    }, bind: function (callback)
     {
-        return [];
-    },
-    curry: function ()
+
+        return [callback, {order: 'PIZZA'}, 'YES'];
+    }, curry: function ()
     {
-        return [];
-    },
-    flip: function ()
+        var func = function (a, b, c)
+        {
+            if (null == a || null == b || null == c) {
+                return false;
+            }
+            return a * b * c;
+        };
+        return [func];
+    }, flip: function ()
     { //use arguments to complete this task
-        return [];
-    },
-    partial: function ()
+        var f = function ()
+        {
+            return _.toArray(arguments);
+        };
+        return [f];
+    }, partial: function ()
     {
-        return [];
-    },
-    rearg: function (callback)
+        var f = function (s1, s2)
+        {
+            return s1 + ' represents ' + s2 + ' character';
+        };
+        return [f, 'Batman', 'GOOD'];
+    }, rearg: function (callback)
     {
-        return [];
-    },
-    spread: function ()
+        return [callback, 1, 3, 0, 2];
+    }, spread: function ()
     {
-        return [];
+        var fun = function (start,stop)
+        {
+            if(start === 'HIV' && stop === 'started'){
+                return 'Treatment due to '+start+' virus has '+stop+'!'
+            } else if(start === 'HIV' && undefined === stop){
+                return 'You were killed by HIV';
+            } else if(start === 'EBOLA' && stop === 'some'){
+                return start +' slowly killing you, I\'m so sorry';
+            } else {
+                return false
+            }
+
+        };
+        return [fun];
     }
 };
