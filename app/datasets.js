be0fca34425c3cf46809f100268f21485d9e5e93
Michal Niedziela <MichalNiedziela1601@labnoratory.com>
2017-02-17 20:55:14 +0100

    test task4

--- app/datasets.js
+++ app/datasets.js
@@ -4,96 +4,99 @@ module.exports = {
 
     add: function ()
     {
-        return [];
+        return [2,-4];
     },
 
     ceil: function ()
     {
-        return [];
+        return [1.8404,2];
     },
 
     divide: function ()
     {
-        return [];
+        return [Number.MIN_VALUE,0];
     },
 
     floor: function ()
     {
-        return [];
+        return [5.4704,2];
     },
 
     max: function ()
     {
-        return [];
+        return [[1,2,3,4,5]];
     },
 
     maxBy1: function ()
     {
-        return [];
+        return [[{a: 2, b: 1}, {a: 3, b: 3}, {a: 1, b: 1}], function(o) { return [o.a, o.b];} ];
     },
 
     maxBy2: function ()
     {
-        return [];
+        return [[{Name: 'Edwin', Gold: 2500}, {name: 'Artur', Gold: 122}, {Name: 'Baltazar', Gold: 1500}],
+            'Name'];
     },
 
     mean: function ()
     {
-        return [];
+        return [[1,3,4,6]];
     },
 
     meanBy1: function ()
     {
-        return [];
+        return [[{n: 30},{n: 27},{n: 24}], function(o){return o.n;}];
     },
 
     meanBy2: function ()
     {
-        return [];
+        return [[{n: 3}, {n: 1}, {n: 2}, {n: 6}], 'n'];
     },
 
     min: function ()
     {
-        return [];
+        return [[9,4,6,-3,-2,0]];
     },
 
     minBy1: function ()
     {
-        return [];
+        return [[{name: 'Adam', age: 33}, {name: 'Justin', age: 21}, {name: 'Baartek', age: 23}],
+        function(o) { return o.age;}];
     },
 
     minBy2: function ()
     {
-        return [];
+        return [[{Name: 'Edwin', Gold: 2500}, {Name: 'Bob', Gold: 200}, {Name: 'Baltazar', Gold: 1500}],
+        'Gold'];
     },
 
     multiply: function ()
     {
-        return [];
+        return [-3,-5];
     },
 
     round: function ()
     {
-        return [];
+        return [8.2457, 3];
     },
 
     subtract: function ()
     {
-        return [];
+        return [15,0];
     },
 
     sum: function ()
     {
-        return [];
+        return [[1,2,3,4]];
     },
 
     sumBy1: function ()
     {
-        return [];
+        return [[{n: 3000}, {n: 300}, {n: 40.5}], function(o) { return o.n;}];
     },
 
     sumBy2: function ()
     {
-        return [];
+        return [[{n: 100}, {n: 50}, { n: 40}, { n: 3}], 'n'];
     }
 };
