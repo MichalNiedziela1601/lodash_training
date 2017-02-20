f32d2a7f3cadecfd38c8c30c9c6284758bfaa782
Michal Niedziela <MichalNiedziela1601@labnoratory.com>
2017-02-20 08:32:37 +0100

    test task5

--- app/datasets.js
+++ app/datasets.js
@@ -4,61 +4,61 @@ module.exports = {
 
     clamp1: function ()
     {
-        return [];
+        return [3,4];
     },
 
     clamp2: function ()
     {
-        return [];
+        return [4,7,3];
     },
 
     clamp3: function ()
     {
-        return [];
+        return [5,2,6];
     },
 
     inRange1: function ()
     {
-        return [];
+        return [3,2,5];
     },
 
     inRange2: function ()
     {
-        return [];
+        return [3,6,9];
     },
 
     inRange3: function ()
     {
-        return [];
+        return [7,1,4];
     },
 
     inRange4: function ()
     {
-        return [];
+        return [4,8];
     },
 
     inRange5: function ()
     {
-        return [];
+        return [-4,5];
     },
 
     inRange6: function ()
     {
-        return [];
+        return [8,4];
     },
 
     inRange7: function ()
     {
-        return [];
+        return [5,5];
     },
 
     random1: function ()
     {
-        return [];
+        return [4,9,true];
     },
 
     random2: function ()
     {
-        return [];
+        return [4,9];
     }
 };
