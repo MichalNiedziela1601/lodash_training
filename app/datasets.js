c3960c303958bb8cb236d1b7f9ed8dbe228c914c
student <michal.niedziela@labnoratory.com>
2017-02-21 10:09:59 +0100

    half test6

--- app/datasets.js
+++ app/datasets.js
@@ -1,106 +1,203 @@
 'use strict';
-
+var _ = require('lodash');
 module.exports = {
 
     assign: function ()
     {
-        return [];
+        function Bar(){
+            this.number = 6;
+            this.string = 'default';
+        }
+
+        Bar.prototype.square = 36;
+        return [{number: 5, square: 25, ping: function() { return 'Number: '+this.number;},
+            pong: function() {return 'Number: '+this.square;}}, new Bar() ];
     },
 
     //alias _.extend
     assignIn: function ()
     {
-        return [];
+        var obj = {
+            number: 5, square: function ()
+            {
+                return 'Square: ' + Math.pow('a', 2);
+            }
+        };
+        function Bar(){
+            this.number = 4;
+            this.cube = 64;
+        }
+
+        Bar.prototype.square = function(){
+            return 'Square: '+Math.pow('b', 2);
+        };
+        Bar.prototype.fourthPower = 256;
+        return [obj, new Bar()];
     },
 
     at1: function (obj)
     {
-        return [];
+        return [obj, ['book[0].other.pages','book[0].count']];
     },
 
     at2: function ()
     {
-        return [];
+        var obj = {
+            0: 'mate',
+        1: 'companion',
+        2: 'fellow',
+        3: 'worker',
+        4: 'friend',
+        5: 'helper'};
+        return [obj, ['0','4']];
     },
 
     defaults: function ()
     {
-        return [];
+        return [{name: 'John',surname: 'Smith',age: 25},
+            {name: 'Tom', gender: 'male'}, {surname: 'Twist'}];
     },
 
     defaultsDeep: function ()
     {
-        return [];
+        var obj = {
+            data: {
+                text: 'Tim',
+                reversedText: 'miT'
+            }
+        };
+        var obj2 = {
+            data: {
+                text: 'mouse',
+                reversedText: 'esuom',
+                lengthText: function(){
+                    return this.text.length;
+                }
+            }
+        };
+        return [obj,obj2];
     },
 
     findKey1: function (obj)
     {
-        return [];
+        return [obj, function(o){return o.number > 20;}];
     },
 
     findKey2: function (obj)
     {
-        return [];
+        var obj2 = {number: 15};
+        return [obj, obj2];
     },
 
     findKey3: function (obj)
     {
-        return [];
+        return [obj,['number', 20]];
     },
 
     findKey4: function (obj)
     {
-        return [];
+        return [obj, 'number'];
     },
 
     findLastKey1: function (obj)
     {
-        return [];
+        return [obj, function(o){ return o.pizza === true;}];
     },
 
     findLastKey2: function (obj)
     {
-        return [];
+        return [obj, { lasagne: false}];
     },
 
     findLastKey3: function (obj)
     {
-        return [];
+        return [obj, ['pasta', true]];
     },
 
     findLastKey4: function (obj)
     {
-        return [];
+        return [obj, 'pizza'];
     },
 
     forIn: function ()
     {
-        return [];
+        function Foo(){
+            this.arms = 2;
+            this.legs = 2;
+        }
+        Foo.prototype.head = 1;
+        return [new Foo(), function(val, key, obj){
+            console.log(obj);
+            obj[key] = val*val;
+            return obj[key];
+        }];
     },
 
     forInRight: function ()
-    {
-        return [];
+    {function Foo(){
+        this.arms = 2;
+        this.legs = 2;
+    }
+        Foo.prototype.head = 1;
+        function fn(val,key,obj){
+            obj[key] = val*2;
+            return obj[key];
+        }
+
+        return [new Foo(), fn];
     },
 
     forOwn: function ()
     {
-        return [];
+        function Foo(){
+            this.r = 7;
+            this.d = 14;
+        }
+        Foo.prototype.l = 4.4;
+        function fn(val,key,obj){
+            if(key === 'r'){
+                obj[key] = val+1;
+                return obj[key];
+            }
+        }
+        return [new Foo(),fn];
     },
 
     forOwnRight: function ()
     {
-        return [];
+        function Foo(){
+            this.r = 7;
+            this.d = 14;
+        }
+        Foo.prototype.l = 4.4;
+        function fn(val,key,obj){
+            if(key === 'r'){
+                obj[key] = val+1;
+                return obj[key];
+            }
+        }
+        return [new Foo(), fn];
     },
 
     functions: function ()
     {
-        return [];
+        function Foo(){
+            this.square = _.constant(4);
+        }
+
+        return [new Foo()];
     },
 
     functionsIn: function ()
     {
-        return [];
+        function Foo(){
+            this.square = _.constant(4);
+        }
+
+        Foo.prototype.cube = function ()
+        {
+        };
+        return [new Foo()];
     },
 
     get1: function (obj)
