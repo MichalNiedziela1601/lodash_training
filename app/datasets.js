4d16e8eec5892f37aa36eaf9f07406906fe7f5a2
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-02-23 10:00:03 +0100

    test task7

--- app/datasets.js
+++ app/datasets.js
@@ -4,61 +4,77 @@ module.exports = {
 
     _: function ()
     {
-        return [];
+        return [[1,2,3,4]];
     },
 
     chain: function ()
     {
-        return [];
+        var users = [
+            {'user': 'Mark', 'gold': 1500000},
+            {'user': 'Freddy', 'gold': 2000000},
+            {'user': 'John', 'gold': 250000}
+        ];
+        return [users];
     },
 
     tap: function ()
     {
-        return [];
+        return [[2,4,6,8],function(o){
+            o.push(10);
+        }];
     },
 
     thru: function ()
     {
-        return [];
+        return ['Roses Are Red', function(val){
+            return new Array(val);
+        }];
     },
 
     _prototype: function ()
     {
-        return [];
+        var wrapped = ['a','b','c','d'];
+        return [wrapped, Array.from(wrapped)];
     },
 
     prototypeAt: function ()
     {
-        return [];
+        return [['Address[0].person.name','Address[0].person.surname',
+            'Address[1].st.number','Address[1].st.name', 'Address[2].city']];
     },
 
     prototypeChain: function ()
     {
-        return [];
+        var users = [
+            {'name': 'Mark', 'age': 26},
+            {'name': 'Jack', 'age': 32},
+            {'name': 'Kate', 'age': 38, 'gender': 'female'}
+        ];
+        return [users];
     },
 
     prototypeCommit: function ()
     {
-        return [];
+        return [['old commit', 'commit']];
     },
 
     prototypeNext: function ()
     {
-        return [];
+        return [['Kate','Smith',30]];
     },
 
     prototypePlant: function ()
     {
-        return [];
+        return [[9,16]];
     },
 
     prototypeReverse: function ()
     {
-        return [];
+        return [[5,3,7,4]];
     },
 
     prototypeValue: function ()
     {
-        return [];
+        return [['Andy', 'Flower', 30, 4600]];
     }
 };
